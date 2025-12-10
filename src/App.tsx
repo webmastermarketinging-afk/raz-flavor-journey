d0c93deebbc2c338e605bfe15fa1cd8ed1e94ac3
webmastermarketinging-afk <webmastermarketinging@gmail.com>
2025-12-10 14:26:42 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
+
 import Index from "./pages/Index";
 import Contact from "./pages/Contact";
 import LabTesting from "./pages/LabTesting";
 import FAQs from "./pages/FAQs";
 import WholesalePortal from "./pages/WholesalePortal";
 import NotFound from "./pages/NotFound";
+
+// 👇 NUEVAS IMPORTACIONES DE PÁGINAS DE PRODUCTO
+import Product2gThcp from "./pages/Product2gThcp";
+import Product2gThca from "./pages/Product2gThca";
+import Product15gThcp from "./pages/Product15gThcp";
 
 const queryClient = new QueryClient();
 
@@ -24,11 +31,21 @@ const App = () => (
         <ScrollToTop />
         <Navigation />
         <Routes>
+          {/* Home */}
           <Route path="/" element={<Index />} />
+
+          {/* Páginas de producto */}
+          <Route path="/products/2g-thcp" element={<Product2gThcp />} />
+          <Route path="/products/2g-thca" element={<Product2gThca />} />
+          <Route path="/products/1-5g-thcp" element={<Product15gThcp />} />
+
+          {/* Otras páginas */}
           <Route path="/lab-testing" element={<LabTesting />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/faqs" element={<FAQs />} />
           <Route path="/wholesale-portal" element={<WholesalePortal />} />
+
+          {/* Catch-all */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
