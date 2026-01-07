429aa6988d3e3713d2250b287a4b56cd99ade388
webmastermarketinging-afk <webmastermarketinging@gmail.com>
2026-01-07 12:00:25 -0500

    Update LabTesting.tsx

--- src/pages/LabTesting.tsx
+++ src/pages/LabTesting.tsx
@@ -111,10 +111,10 @@ const productsTHCP2g = [
 const productsTHCA2g = [
   {
     id: 1,
-    name: "Emerald City",
+    name: "Esmerald City",
     type: "Sativa",
     images: [thca2gEmeraldCity],
-    gradient: "from-emerald-500 to-green-600",
+    gradient: "from-esmerald-500 to-green-600",
     pdfUrl: "#",
   },
   {
